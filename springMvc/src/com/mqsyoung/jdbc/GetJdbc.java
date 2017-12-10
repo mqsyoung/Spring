f08f099d66a2d788205238622bfe93f0f973e29f
lenovo <lenovo@192.168.1.105>
2017-12-11 01:47:44 +0800

    链接阿里云mysql数据库

---
+++ springMvc/src/com/mqsyoung/jdbc/GetJdbc.java
@@ -0,0 +1,60 @@
+package com.mqsyoung.jdbc;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+/**
+ * 获取和释放数据库连接
+ * @author lenovo
+ *
+ */
+
+
+public class GetJdbc {
+	
+	private static DataSource dataSource = new ComboPooledDataSource("webDataSource");
+	
+	/**
+	 * 获取数据库链接
+	 * @return
+	 */
+	public static Connection getConection(){
+		
+		Connection conn =null;
+		
+		try {
+			conn = dataSource.getConnection();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		
+		return conn;
+	}
+	
+	/**
+	 * 释放数据库连接
+	 * @param conn
+	 */
+	public static void releaseConection(Connection conn){
+		
+		if(conn != null){
+			try {
+				
+				conn.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
+	
+	
+}
---
+++ springMvc/src/com/mqsyoung/util/JdbcUtil.java
@@ -0,0 +1,32 @@
+package com.mqsyoung.util;
+
+import java.sql.Connection;
+
+import org.junit.Test;
+
+import com.mqsyoung.jdbc.GetJdbc;
+
+public class JdbcUtil {
+
+	
+	  @Test
+	  public void testGetJdbc(){
+		  Connection conn = null;
+		  try {
+			  conn =GetJdbc.getConection();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		  
+		  if(conn != null){
+			  
+			  System.out.println(conn);
+		  }else{
+			  System.out.println("不好意识，没有获取到数据源……");
+		  }
+		  
+		  
+	  }
+}
