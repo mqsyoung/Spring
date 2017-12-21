914953dc537facb674d10ccfbf8a99622b558321
mengqingsen <mengqingsen@mengqingsen-lwb.sinosig.com>
2017-12-21 18:11:06 +0800

    commit

--- springMvc/src/com/mqsyoung/controller/UserController.java
+++ springMvc/src/com/mqsyoung/controller/UserController.java
@@ -1,17 +1,25 @@
 package com.mqsyoung.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mqsyoung.entity.User;
+import com.mqsyoung.service.UserService;
 
 @Controller
 public class UserController {
 
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping("test")
 	public String testSpringMvc(){
 		
 		System.out.println("123");
+		User user = userService.getUser();
 		
+		System.out.println(user);
 		return "success";
 	}
 }
